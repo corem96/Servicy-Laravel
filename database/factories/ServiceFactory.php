dfb884b4a5eb5b1a87baac980505a0417f8b3317
jorge escamilla <corem96@gmail.com>
2019-02-12 14:25:17 -0700

    Update service factory

--- database/factories/ServiceFactory.php
+++ database/factories/ServiceFactory.php
@@ -4,8 +4,8 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Service::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'name' => $faker->word,
         'details' => $faker->text,
-        'price' => $faker->randomFloat()
+        'price' => $faker->randomFloat(2, 0, null)
     ];
 });
