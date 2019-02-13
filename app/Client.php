e6163ebee4b7c024af7044ff4f924d3046bb2f2e
jorge escamilla <corem96@gmail.com>
2019-02-13 16:57:40 -0700

    Update models including relationship methods for appointment

--- app/Client.php
+++ app/Client.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends Model
 {
     protected $fillable = ['name', 'mobile', 'email'];
+
+    public function appointment()
+    {
+        return $this->hasMany(App\Appointment::class);
+    }
 }
--- app/Service.php
+++ app/Service.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Service extends Model
 {
     protected $fillable = ['name', 'details', 'price'];
+
+    public function appointments()
+    {
+        return $this->belongsToMany(App\Appointments::class, 'service_requested')
+            ->withPivot('service_id', 'price', 'is_provided')
+            ->withTimestamps();
+    }
 }
