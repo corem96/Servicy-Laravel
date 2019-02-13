e63dcf095be116cc1812eb1d9c2b2e15b41406db
jorge escamilla <corem96@gmail.com>
2019-02-13 16:55:25 -0700

    Update relationship methods for services and clients

--- app/Appointment.php
+++ app/Appointment.php
@@ -7,16 +7,31 @@ use Illuminate\Database\Eloquent\Model;
 class Appointment extends Model
 {
     protected $fillable = [
+        'client_id',
+        'user_id_created',
+        'user_id',
         'client_name',
         'client_contact',
         'price_expected',
         'price_full',
-        'price_final',
         'discount',
+        'price_final',
         'canceled',
         'cancelation_reason',
         'start_at',
         'ends_at_expected',
-        'ends_at'
+        'ends_at',
     ];
+
+    public function services()
+    {
+        return $this->belongsToMany(App\Service::class, 'service_requested')
+            ->withPivot('appointment_id', 'price', 'is_provided')
+            ->withTimestamps();
+    }
+
+    public function clients()
+    {
+        return $this->belongsTo(App\Client::class);
+    }
 }
