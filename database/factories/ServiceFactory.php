42c9182e7f27292a5c328b5601c55b02e3d6014a
Jorge Escamilla <corem96@gmail.com>
2019-02-27 23:38:22 -0700

    Update service faker limiting max float number for price field

--- database/factories/ServiceFactory.php
+++ database/factories/ServiceFactory.php
@@ -6,6 +6,6 @@ $factory->define(App\Service::class, function (Faker $faker) {
     return [
         'name' => $faker->word,
         'details' => $faker->text,
-        'price' => $faker->randomFloat(2, 0, null)
+        'price' => $faker->randomFloat(2, 0, 99999999)
     ];
 });
